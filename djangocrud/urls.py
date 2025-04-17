a3c628e67e4c831b0da0da12740d5c9f000a9747
94leonardo <lassoleonardo56@gmail.com>
2025-04-17 01:43:20 -0500

    signup passowrd y user

--- djangocrud/urls.py
+++ djangocrud/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from tasks import views
 
+"""rutas"""
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.HelloWord),
-    path('signup/', views.HelloWord)
+    path('', views.home, name = "home"),
+    path('signup/', views.signup, name="signup")
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here. ruta de las vistas
 
-def HelloWord(request):
-    title = 'Hello Word'
-    return render(request, 'signup.html', {
-        'form': UserCreationForm
+def home(request):
+   
+    return render(request, 'home.html')
+def signup(request):
+    if request.method == 'GET':
+        return render(request, 'signup.html',{
+        'form': UserCreationForm              
     })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            #registro user
+            try:
+                user = User.objects.create_user(
+                username=request.POST['username'],
+                password=request.POST['password1'])
+
+                user.save()
+                return HttpResponse('User create susserfully')
+            except:
+                
+                return HttpResponse('User alredy exist')
+
+            
+        return HttpResponse('Password do not match')
+    
+    
