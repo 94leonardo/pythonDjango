6a5a14c450f5c9221610255323e38a498c7a23d3
94leonardo <lassoleonardo56@gmail.com>
2025-05-08 00:22:13 -0500

    manejo de errores

--- tasks/views.py
+++ tasks/views.py
@@ -1,33 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 
 # Create your views here. ruta de las vistas
 
+
 def home(request):
-   
-    return render(request, 'home.html')
+
+    return render(request, "home.html")
+
+
 def signup(request):
-    if request.method == 'GET':
-        return render(request, 'signup.html',{
-        'form': UserCreationForm              
-    })
+    if request.method == "GET":
+        return render(request, "signup.html", {"form": UserCreationForm})
     else:
-        if request.POST['password1'] == request.POST['password2']:
-            #registro user
+        if request.POST["password1"] == request.POST["password2"]:
+            # registro user
             try:
                 user = User.objects.create_user(
-                username=request.POST['username'],
-                password=request.POST['password1'])
+                    username=request.POST["username"],
+                    password=request.POST["password1"],
+                )
 
                 user.save()
-                return HttpResponse('User create susserfully')
+                return redirect("tasks")
             except:
-                
-                return HttpResponse('User alredy exist')
 
-            
-        return HttpResponse('Password do not match')
-    
-    
+                return render(
+                    request,
+                    "signup.html",
+                    {"form": UserCreationForm, "error": "Username already exists"},
+                )
+
+        return render(
+            request,
+            "signup.html",
+            {"form": UserCreationForm, "error": "Password do not match"},
+        )
+
+
+def tasks(request):
+    return render(request, "tasks.html")
